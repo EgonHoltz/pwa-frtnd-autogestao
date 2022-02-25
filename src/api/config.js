6ca4082c161da524d94eb2ff783203de27d34765
Egon Holtz <egonholtz@gmail.com>
2022-02-25 14:03:59 +0000

    changes configs to make it available to internet

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
 //const API_URL = "http://localhost:8010";
-const API_URL = "https://mighty-depths-32331.herokuapp.com";
+const API_URL = "http://mighty-depths-32331.herokuapp.com";
 export default API_URL;
